2c3870dcbb2a0725691f5d9b9fec94a66cbbd217
Wilson Neto <wilson.neto@encripta.com.br>
2019-10-24 19:03:38 -0300

    testando nova versão

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>V. 0.2.3</p>
+        <p>V. 0.2.4</p>
         <a
           className="App-link"
           href="https://reactjs.org"
