dab721a8418ef6d2820713a191d009a846de1d22
Wilson Neto <wilson.neto@encripta.com.br>
2019-10-24 09:47:14 -0300

    0.2.3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>V. 0.2.1</p>
+        <p>V. 0.2.3</p>
         <a
           className="App-link"
           href="https://reactjs.org"
